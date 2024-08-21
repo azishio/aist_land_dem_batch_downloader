6d4bd34cfa2d2a3c187e412d05627d2b9ea9f33a
azishio <azishio3141@gmail.com>
2024-08-21 15:51:26 +0900

    🐛 横方向の画素の接続を正常化

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+use image::codecs::jpeg;
 use isahc::ReadResponseExt;
 use memmap2::MmapOptions;
-use rayon::iter::{IntoParallelIterator, ParallelIterator};
 use std::fs::OpenOptions;
 use std::io::Cursor;
 
@@ -11,11 +11,12 @@ use image::ImageReader;
 #[cfg(feature = "mpi")]
 use mpi::traits::Communicator;
 
-pub const ZOOM_LV: u32 = 4;
+pub const ZOOM_LV: u32 = 3;
 pub const OUTPUT_BIN_PATH: &str = "./result.bin";
+pub const OUTPUT_JPG_PATH: &str = "./result.jpg";
 
-const IMAGE_SIZE: u64 = 256;
-const NUM_OF_PIXELS: u64 = IMAGE_SIZE * IMAGE_SIZE;
+const TILE_SIZE: u64 = 256;
+const NUM_OF_PIXELS: u64 = TILE_SIZE * TILE_SIZE;
 
 #[cfg(feature = "mpi")]
 fn mpi_init() -> (usize, usize) {
@@ -33,7 +34,7 @@ fn mpi_init() -> (u64, u64) {
 }
 
 // 画像を取得して標高データを返す
-fn fetch(x: u64, y: u64) -> anyhow::Result<Vec<f32>> {
+fn fetch(x: u64, y: u64) -> anyhow::Result<Vec<Vec<f32>>> {
     let url = format!("https://tiles.gsj.jp/tiles/elev/land/{ZOOM_LV}/{y}/{x}.png");
     // いちいちクライアントを作るので遅い
     let mut res = isahc::get(&url).expect("Failed to get");
@@ -67,12 +68,15 @@ fn fetch(x: u64, y: u64) -> anyhow::Result<Vec<f32>> {
                     f32::MIN
                 }
             })
+            .collect::<Vec<_>>()
+            .chunks(TILE_SIZE as usize)
+            .map(|chunk| chunk.to_vec())
             .collect();
 
         Ok(altitude)
     } else {
         // タイル全体が存在しない場合
-        Ok(vec![f32::MIN; NUM_OF_PIXELS as usize])
+        Ok(vec![vec![f32::MIN; TILE_SIZE as usize]; TILE_SIZE as usize])
     }
 }
 
@@ -107,12 +111,30 @@ fn main() -> anyhow::Result<()> {
     // タイル群の幅
     let weidth = 2_u64.pow(ZOOM_LV);
 
-    let result_bytes = (start_h..end_h)
-        .into_par_iter()
-        .flat_map(|y| (0..weidth).into_par_iter().flat_map(move |x| fetch(x, y)))
-        .flatten()
-        .flat_map(|f| f.to_be_bytes())
-        .collect::<Vec<_>>();
+    let big_tile = {
+        let mut field =
+            vec![vec![f32::MIN; (weidth * TILE_SIZE) as usize]; (height * TILE_SIZE) as usize];
+
+        // 256x256の標高タイルの配列
+        (start_h..end_h).into_iter().for_each(|tile_y| {
+            (0..weidth).into_iter().for_each(|tile_x| {
+                if let Ok(tile) = fetch(tile_x, tile_y) {
+                    tile.into_iter().enumerate().for_each(|(pixel_y, row)| {
+                        row.into_iter().enumerate().for_each(|(pixel_x, altitude)| {
+                            let y = (tile_y * TILE_SIZE) as usize + pixel_y;
+                            let x = (tile_x * TILE_SIZE) as usize + pixel_x;
+                            field[y as usize][x as usize] = altitude;
+                        })
+                    })
+                }
+            })
+        });
+
+        field
+            .into_iter()
+            .flat_map(|row| row.into_iter().flat_map(|f| f.to_be_bytes()))
+            .collect::<Vec<_>>()
+    };
 
     let file = OpenOptions::new()
         .read(true)
@@ -131,13 +153,34 @@ fn main() -> anyhow::Result<()> {
     let start_ptr = mmap.as_mut_ptr();
 
     unsafe {
-        result_bytes
-            .into_iter()
-            .enumerate()
-            .for_each(|(offset, byte)| {
-                let ptr = start_ptr.add(offset);
-                ptr.write(byte);
-            });
+        big_tile.iter().enumerate().for_each(|(offset, &byte)| {
+            let ptr = start_ptr.add(offset);
+            ptr.write(byte);
+        });
     }
+
+    let pixels = big_tile
+        .chunks(4)
+        .map(|chunk| {
+            let float = f32::from_be_bytes(chunk.try_into().unwrap()) as f64;
+
+            // 0から8848(エベレスト)までの値に正規化
+            let normalized = (float / 8848.0) * 255.0;
+            normalized as u8
+        })
+        .collect::<Vec<_>>();
+
+    let jpg = File::create(OUTPUT_JPG_PATH)?;
+    let mut jpg_encoder = jpeg::JpegEncoder::new(jpg);
+    let image_size = TILE_SIZE * 2_u64.pow(ZOOM_LV);
+    jpg_encoder
+        .encode(
+            pixels.as_slice(),
+            image_size as u32,
+            image_size as u32,
+            image::ExtendedColorType::L8,
+        )
+        .unwrap();
+
     Ok(())
 }
